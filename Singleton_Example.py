e8841d52fea59d566b85326d565f192c77fd69bf
Jabrail <lezgintsev13@yandex.ru>
2017-12-02 21:46:30 +0300

    Singleton

---
+++ Singleton_Example.py
@@ -0,0 +1,33 @@
+def singleton(klass):
+
+    instances = None
+
+    def wrap(*args):
+        nonlocal instances
+
+        if instances == None:
+            instances = klass(*args)
+
+        return instances
+
+    return wrap
+
+
+@singleton
+class Person:  # Присвоит onCall имени Person
+
+    def __init__(self, name, hours, rate):  # onCall сохранит Person
+        self.name = name
+        self.hours = hours
+        self.rate = rate
+
+    def pay(self):
+        return self.hours * self.rate
+
+
+z = Person('Bob', 14, 4)
+x = Person('Jack', 44, 4)
+
+print(z, x)
+
+print(z.hours, x.hours)
